1de169bf3b35d80b4f610732cf681b708badf10d
Brian Zhu <brzx@foxmail.com>
2019-03-15 16:38:13 +0800

    Add files via upload

---
+++ walkfolder.py
@@ -0,0 +1,11 @@
+import os
+
+pt = os.path.abspath(os.curdir)
+li = list(os.walk(pt))
+
+with open('movewalks.txt','w') as f:
+    for tp in li:
+        a, b, c = tp
+        f.write(str(a) + '\n')
+        for it in c:
+            f.write('    ' + str(it) + '\n')
